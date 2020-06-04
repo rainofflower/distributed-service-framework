8659eac0eb1fc485003ffc6a4cb46720087b72b5
yanghui <yanghui@linklogis.com>
2020-06-05 00:07:43 +0800

    线程池处理任务同步等待处理完成逻辑 模板代码

--- src/test/java/PromiseTest.java
+++ src/test/java/PromiseTest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
 /**
@@ -123,5 +124,55 @@ public class PromiseTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void test4(){
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(5,
+                10,
+                60,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(100));
+        AtomicInteger atomicInteger = new AtomicInteger();
+        int total = 380;
+        int batchCount = 110;
+        int perCost = 100;
+        int batch = total % batchCount == 0 ? total / batchCount : total / batchCount + 1;
+        System.out.println("batch num: "+batch);
+        for(int b=0; b<batch; b++){
+            long start2 = System.currentTimeMillis();
+            int min = Math.min((b + 1) * batchCount, total);
+            CountDownLatch latch = new CountDownLatch(min-(batchCount*b));
+            for(int i = batchCount*b; i<min; i++){
+                try{
+                    pool.execute(()->{
+                        try{
+                            work(perCost);
+                            atomicInteger.incrementAndGet();
+                        }finally {
+                            latch.countDown();
+                        }
+                    });
+                }catch (Throwable throwable){
+                    throwable.printStackTrace();
+                    latch.countDown();
+                }
+            }
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("run "+atomicInteger.get() + " work\ncost "+(System.currentTimeMillis() - start2)+" ms\n" + "serial cost "+total*perCost);
+            System.out.println("----------");
+        }
+        System.out.println("==========");
+        System.out.println("total run "+atomicInteger.get());
+    }
+
+
+    public void work(int mills){
+        LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(mills));
     }
 }
